58a5e7cc5dbc07d6c91b4cc59b2d605c2156436c
Matias Aspis <mhaspis@gmail.com>
2020-09-19 14:36:13 -0300

    Controllers, models y relaciones

---
+++ app/ProductIngredient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductIngredient extends Model
+{
+    protected $table = 'product_ingredient';
+
+    public function products(){
+        return $this->hasMany(Product::class, 'product_id');
+    }
+
+    public function ingredients(){
+        return $this->hasMany(Ingredient::class, 'ingredient_id');
+    }
+}
