41ff0f6aa12c48f750e22a5799a9a4b76a694791
Matias Aspis <mhaspis@gmail.com>
2020-09-19 12:56:54 -0300

    DB y migrations

---
+++ database/migrations/2020_09_19_142920_create_ingredients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('mark')->nullable();           
+            $table->enum('measure', ['kg', 'ml'])->nullable();
+            $table->decimal('weight', 8, 2)->nullable();
+            $table->decimal('cost', 8, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
---
+++ database/migrations/2020_09_19_143124_create_product_ingredient_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductIngredientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_ingredient', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('ingredient_id')->unsigned();
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->decimal('used_weight', 8, 3)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_ingredient');
+    }
+}
